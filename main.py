55177bb70ae0908c79d9329b9e4b89ac048a0bcd
Nicolas Bartual <njbarmon@epsa.upb.es>
2024-03-23 12:21:47 +0100

    File selecction added

--- main.py
+++ main.py
@@ -9,4 +9,17 @@ for file in os.listdir(folder_path):
     if file.endswith(".txt"):
         CLASSES.append(os.path.splitext(file)[0])
 
-print(CLASSES)
+while(True):
+    print(CLASSES)
+    file_path = folder_path + "/" +input("Select a file: ") + ".txt"
+    print(file_path)
+
+    try:
+        with open(file_path) as file:
+            content = file.read()
+    except FileNotFoundError:
+        print("Error: file doesn't exist")
+    else:
+        break
+
+print(content)
